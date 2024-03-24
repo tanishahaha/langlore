b5d71f84ab07f9d934ea990e5f3e0bdf563af3f9
Tanisha Amin <amintanisha17@gmail.com>
2024-03-24 17:46:53 +0530

    test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar: React.FC = () => {
           </div>
           <div className="hidden lg:flex items-center gap-[32px] text-[1.2rem]">
             <Link to="/dj" className="text-white hover:text-gray-300">Courses</Link>
-            <Link to="/about" className="text-white hover:text-gray-300" >About Us</Link>
+            <a href="/about" className="text-white hover:text-gray-300" >About Us</a>
             <Link to="/blog" className="text-white hover:text-gray-300" >Blog</Link>
             <Link to="/signin" className="bg-bluee px-6 py-2 rounded-[16px] hover:shadow-inner text-[1.5rem] tracking-wider text-white hover:text-pri">Sign in</Link>
           </div>
@@ -47,7 +47,7 @@ const Navbar: React.FC = () => {
         <MdClose size={30} />
       </button>
       <Link to="/jh" className="block py-2 px-4 text-white hover:bg-gray-700">Courses</Link>
-      <Link to="/about" className="block py-2 px-4 text-white hover:bg-gray-700">About Us</Link>
+      <a href="/about" className="block py-2 px-4 text-white hover:bg-gray-700">About Us</a>
       <Link to="/blog" className="block py-2 px-4 text-white hover:bg-gray-700" >Blog</Link>
       <Link to='/signin' className="bg-bluee px-8 py-4 rounded-[16px] hover:shadow-xl text-[2rem] tracking-wide text-white  max-md:text-mdsubsubheading max-md:py-2" >
         Signup
