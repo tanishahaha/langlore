9542c9b2e04910548b9c5eadb9ebdb55f8947a78
Shivangi Chaurasiya <chaurasiyashivangi7@gmail.com>
2024-04-09 20:48:22 +0530

    blog update

--- src/components/AboutUsPage/AboutCards.tsx
+++ src/components/AboutUsPage/AboutCards.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { FaLinkedin, FaTwitter, FaGlobe } from 'react-icons/fa';
+import { FaLinkedin, FaTwitter } from 'react-icons/fa';
+
 interface TeamMemberProps {
     name: string;
     title: string;
     description: string;
     linkedin: string;
     twitter: string;
-    website: string;
     image: string;
 }
 
-const TeamMember: React.FC<TeamMemberProps> = ({ name, title, description, linkedin, twitter, website, image }) => {
+const TeamMember: React.FC<TeamMemberProps> = ({ name, title, description, linkedin, twitter, image }) => {
     return (
         <div className="flex text-gray-300  flex-col items-center p-4 ">
             <img src={image} alt={`Profile picture of ${name}`} className="mb-4 rounded-full w-[300px] h-[300px]" />
@@ -18,9 +18,14 @@ const TeamMember: React.FC<TeamMemberProps> = ({ name, title, description, linke
             <p className="text-mdsubheading font-medium ">{title}</p>
             <p className="text-center w-2/3 mt-3 text-mdsubheading">{description}</p>
             <div className="flex space-x-3 mt-4">
-                <a href={linkedin} className="text-white hover:text-gray-100" target="_blank"><FaLinkedin size={24}/></a>
-                <a href={twitter} className="text-white hover:text-gray-100" target="_blank"><FaTwitter size={24}/></a>
-                <a href={website}  className="text-white hover:text-gray-100 " target="_blank"><FaGlobe size={24}/></a>
+                <a href={linkedin} className="text-white hover:text-gray-100" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin size={24} />
+                </a>
+                {twitter && ( // Render Twitter icon only if twitter link is provided
+                    <a href={twitter} className="text-white hover:text-gray-100" target="_blank" rel="noopener noreferrer">
+                        <FaTwitter size={24} />
+                    </a>
+                )}
             </div>
         </div>
     );
--- src/components/Blog_Page/Blog_Hero.tsx
+++ src/components/Blog_Page/Blog_Hero.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Card from '../Cards';
 import blog from '../../../public/imgs/blog_card_img.png'
+import post1 from "../../../public/imgs/post1.jpg";
+import post2 from "../../../public/imgs/post2.jpg";
+import post3 from "../../../public/imgs/post3.jpg";
+import post4 from "../../../public/imgs/post4.jpg";
+import post5 from "../../../public/imgs/post5.jpg";
+import { Link } from "react-router-dom";
 
 const Blog_Hero: React.FC = () => {
     return (
@@ -9,96 +15,113 @@ const Blog_Hero: React.FC = () => {
                 <h1 className="text-lgheading max-md:text-mdheading font-bold mt-9 text-white">Discover our Latest Blogs</h1>
                 <p className="text-gray-400 text-mdsubheading mb-14 mt-3">Stay updated about the cultures of endangered languages</p>
             </div>
-            
+
             <div className="">
                 <div className="md:grid grid-cols-3 gap-6"> {/* Grid with three columns */}
                     <div className=''>
                         <Card
-                            title="The Beauty of Language Diversity"
-                            subtitle="Exploring the richness of linguistic diversity around the world"
+                            title="Why do Mangaloreans love Pabba's?"
+                            subtitle="What began as a humble venture in 1975 evolved into a beloved institution"
                             category="Language"
-                            author="Jane Smith"
-                            date="11 Jan 2022"
+                            author="Mohammed Rayaan"
+                            date="31 March 2024"
                             readTime="7 min read"
-                            imageUrl={blog}
+                            imageUrl={post1}
                             index={1}
-                            altText="Mobile phone displaying LangLore app with text 'Unlocking Languages | Exploring Cultures'"
+                            altText="Why do Mangaloreans love Pabba's?"
+                            linkUrl="https://langlore.substack.com/p/why-do-mangaloreans-love-pabbas?utm_source=profile&utm_medium=reader2"
                         />
+
                     </div>
                     <div className=''>
                         <Card
-                            title="The Beauty of Language Diversity"
-                            subtitle="Exploring the richness of linguistic diversity around the world"
+                            title="What we saw in our first 'Bhoota Kola'"
+                            subtitle="Bhoota Kola is proof of the tenacity of Mangalore's cultural legacy"
                             category="Language"
-                            author="Jane Smith"
-                            date="11 Jan 2022"
+                            author="Zahanat Khan"
+                            date="24 March 2024"
                             readTime="7 min read"
-                            imageUrl={blog}
+                            imageUrl={post2}
                             index={2}
-
-                            altText="Mobile phone displaying LangLore app with text 'Unlocking Languages | Exploring Cultures'"
+                            altText="What we saw in our first 'Bhoota Kola'"
+                            linkUrl="https://langlore.substack.com/p/what-we-saw-in-our-first-bhoota-kola?utm_source=profile&utm_medium=reader2"
                         />
+
                     </div>
                     <div className=''>
                         <Card
-                            title="The Beauty of Language Diversity"
-                            subtitle="Exploring the richness of linguistic diversity around the world"
+                            title="How non-English language cinema is altering the Oscars landscape"
+                            subtitle="Film history scholar says its recent rise in recognition is attributed to industry demographic shifts"
                             category="Language"
-                            author="Jane Smith"
-                            date="11 Jan 2022"
+                            author="Unknown"
+                            date="17 March 2024"
                             readTime="7 min read"
-                            imageUrl={blog}
+                            imageUrl={post3}
                             index={3}
-
-                            altText="Mobile phone displaying LangLore app with text 'Unlocking Languages | Exploring Cultures'"
+                            altText="How non-English language cinema is altering the Oscars landscape"
+                            linkUrl="https://langlore.substack.com/p/post-6"
                         />
+
                     </div>
                 </div>
                 <div className="md:grid grid-cols-3 gap-6"> {/* Grid with three columns */}
                     <div className=''>
+
                         <Card
-                            title="The Beauty of Language Diversity"
-                            subtitle="Exploring the richness of linguistic diversity around the world"
+                            title="Digital Preservation: Can Tech Save Indigenous Languages?"
+                            subtitle="Taking Indigenous languages online: can they be seen, heard and saved?"
                             category="Language"
-                            author="Jane Smith"
-                            date="11 Jan 2022"
+                            author="Unknown"
+                            date="25 Feb 2024"
                             readTime="7 min read"
-                            imageUrl={blog}
+                            imageUrl={post4}
                             index={4}
-
-                            altText="Mobile phone displaying LangLore app with text 'Unlocking Languages | Exploring Cultures'"
+                            altText="Digital Preservation: Can Tech Save Indigenous Languages?"
+                            linkUrl="https://langlore.substack.com/p/post-3?utm_source=profile&utm_medium=reader2"
                         />
                     </div>
                     <div className=''>
+
                         <Card
-                            title="The Beauty of Language Diversity"
-                            subtitle="Exploring the richness of linguistic diversity around the world"
+                            title="Dune's Language Dynamics: The Power of Signs"
+                            subtitle="Sign language can be both a hiding place and a tool - for survival, and for empowerment"
                             category="Language"
-                            author="Jane Smith"
-                            date="11 Jan 2022"
+                            author="Unknown"
+                            date="10 March 2024"
                             readTime="7 min read"
-                            imageUrl={blog}
+                            imageUrl={post5}
                             index={5}
-
-                            altText="Mobile phone displaying LangLore app with text 'Unlocking Languages | Exploring Cultures'"
+                            altText="Dune's Language Dynamics: The Power of Signs"
+                            linkUrl="https://langlore.substack.com/p/dunes-language-dynamics-the-power?utm_source=profile&utm_medium=reader2"
                         />
+
                     </div>
                     <div className=''>
+
                         <Card
-                            title="The Beauty of Language Diversity"
-                            subtitle="Exploring the richness of linguistic diversity around the world"
+                            title="How to improve multilingual reading instruction"
+                            subtitle=
+                            "Learning to read in another language is tough: how Namibian teachers can help kids"
+
                             category="Language"
-                            author="Jane Smith"
-                            date="11 Jan 2022"
+                            author="Unknown"
+                            date="05 March 2024"
                             readTime="7 min read"
                             imageUrl={blog}
                             index={6}
+                            altText="How to improve multilingual reading instruction"
 
-                            altText="Mobile phone displaying LangLore app with text 'Unlocking Languages | Exploring Cultures'"
-                        />
+                            linkUrl="/https://langlore.substack.com/p/post-4?utm_source=profile&utm_medium=reader2" />
+
                     </div>
                 </div>
             </div>
+            <Link to="https://substack.com/profile/202583695-langlore" target="_blank" rel="noopener noreferrer">
+                <button className="custom-button mt-5 md:text-[18px] text-[13px]" data-aos="fade-up">
+                    View All
+                </button>
+            </Link>
+
         </div>
     );
 }
