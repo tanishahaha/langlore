d9428e97a7e5f62e174de4666292d4268f10ceee
Shivangi Chaurasiya <chaurasiyashivangi7@gmail.com>
2024-04-09 21:55:36 +0530

    blog update

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
+import "./component.css"; // Import CSS file for Card component
 
 interface CardProps {
   title: string;
@@ -45,27 +46,18 @@ const Card: React.FC<CardProps> = ({
   return (
     <a href={linkUrl} target="_blank" rel="noopener noreferrer">
       <div
-        className={`custom-inputColor rounded-3xl border-t border-t-white h-[73vh] bg-gradient-to-t from-gray-800 to-transparent overflow-hidden shadow-lg m-3 max-md:my-3 max-md:mx-0 px-8 py-10 max-w-md max-md:py-8 max-md:px-6 tracking-wider max-md:w-full `}
-        data-aos={animationData}
+        className={`card ${animationData} custom-inputColor bg-gradient-to-t h-fit from-gray-800 to-transparent shadow-lg tracking-wider`} 
       >
-        <img className="w-full h-[13rem]  rounded-xl" src={imageUrl} alt={altText} />
-        <div className="pt-6">
-          <div className="bg-gray-600 rounded-xl px-5 py-2 inline-block text-gray-300 text-mdsubsubheading">
-            {category}
-          </div>
-          <div className="font-semibold py-2 text-lgsubsubheading max-md:text-mdsubheading">
-            {title}
-          </div>
-          <div className="text-gray-400 text-mdsubheading max-md:text-mdsubsubheading">
-            {subtitle}
-          </div>
-          <div className="flex gap-3 items-center mt-5 ">
-            <FaUserCircle className="text-white" size={24} />
-            <div className="text-mdsubheading space-y-1">
-              <p className="leading-none text-bluee">{author}</p>
-              <p className="text-white text-mdsubsubheading">
-                {date} · {readTime}
-              </p>
+        <img className="image" src={imageUrl} alt={altText} />
+        <div className="content">
+          <div className="category bg-gray-600">{category}</div>
+          <div className="title">{title}</div>
+          <div className="subtitle text-gray-400">{subtitle}</div>
+          <div className="author-info">
+          <FaUserCircle className="text-white" size={24} />
+            <div className="author">
+              <p className="author-name text-bluee">{author}</p>
+              <p className="date-readTime">{date} · {readTime}</p>
             </div>
           </div>
         </div>
