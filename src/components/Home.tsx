2ca9de1ac44d1ca58fc788ab3fb59381fe3befc6
Shivangi Chaurasiya <chaurasiyashivangi7@gmail.com>
2024-04-12 03:03:27 +0530

    website

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,6 @@ import CourseCard from "./CourseCard";
 import NewsLetter from "./NewsLetter";
 import Sponser from "./Sponser";
 import BlogCards from "./BlogCards";
-import PaymentButton from "./Payment_popup";
 // import BackGround from "./BackGround";
 const Home = () => {
   
@@ -31,9 +30,7 @@ const Home = () => {
       <div className="mt-20">
         <GetInTouch />
       </div>
-      <div className="mt-20">
-        <PaymentButton />
-      </div>
+      
 
       <div className="h-[20vh] bg-pri blur-md max-sm:h-[10vh]"></div>
     </div>
