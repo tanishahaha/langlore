f659622231a8f56b2132069b96cf448aba1806c3
Shivangi Chaurasiya <chaurasiyashivangi7@gmail.com>
2024-04-14 01:13:48 +0530

    Team

--- src/components/AboutUsPage/AboutTeamCards.tsx
+++ src/components/AboutUsPage/AboutTeamCards.tsx
@@ -6,6 +6,12 @@ import Rayaan from '../../../public/imgs/Mohammed_Rayaan.jpg'
 import Deekshika from '../../../public/imgs/Deekshika_Ganesh.jpg'
 import Zahanat from '../../../public/imgs/Zahanat_Khan.jpeg'
 import Madhuja from '../../../public/imgs/Madhuja_Joshi.jpg'
+import Vedant from '../../../public/imgs/Vedant_Jha.jpeg'
+import Shivangi from '../../../public/imgs/Shivangi_Chaurasiya.jpg'
+import Tanisha from '../../../public/imgs/Tanisha_Amin.jpg'
+import Ruchit from '../../../public/imgs/Ruchit_Mehta.png'
+import Vinay from '../../../public/imgs/Vinaykumar_Choursiya.jpeg'
+import Sangeet from '../../../public/imgs/Sangeet_Muralidhar.jpeg'
 
 
 const Team: React.FC = () => {
@@ -20,7 +26,7 @@ const Team: React.FC = () => {
           Passionate individuals dedicated to our mission.
         </p>
       </div>
-      <div className="flex flex-wrap justify-center  w-full items-center text-center max-md:flex-col ">
+      <div className="flex flex-wrap justify-center  w-full  items-center text-center max-md:flex-col ">
         <div className="lg:w-1/3 md:w-full  mb-8 ">
           <TeamMember
             name="Eisha Sathish Shetty"
@@ -48,7 +54,7 @@ const Team: React.FC = () => {
           <TeamMember
             name="Mohammed Rayaan"
             title="Content Curation | Hyperlocal Journalism"
-            description="Rayaan is passionate about using solution-based hyperlocal journalism and education to empower marginalized communities, providing valuable information to inspire actionable life choices."
+            description="Rayaan is passionate about using solution-based hyperlocal journalism & education to empower communities, providing valuable information to inspire actionable life choices."
             linkedin="https://www.linkedin.com/in/mohammed-rayaan-232887135/"
             twitter=""
             
@@ -70,7 +76,7 @@ const Team: React.FC = () => {
           <TeamMember
             name="Zahanat Khan"
             title="Content Curation | Research and Operations"
-            description="Zahanat sees value in the power of literature and the vision of LangLore and its application to her own culture."
+            description="Zahanat sees value in the power of literature and the vision of LangLore and its enrich the cultural landscape application to her own culture."
             linkedin="https://www.linkedin.com/in/zahanat-21a77623a/"
             twitter=""
             
@@ -88,6 +94,73 @@ const Team: React.FC = () => {
             image={Madhuja}
           />
         </div>
+        <div className="lg:w-1/3 md:w-full  mb-8">
+          <TeamMember
+            name="Sangeet Muralidhar"
+            title="Head of Engineering"
+            description="Sangeet brings in his technological prowess to ensure the engineering team is free of bugs or blockers, and sites like LangLore get built securely."
+            linkedin="https://www.linkedin.com/in/sangeet-muralidhar/"
+            twitter="https://twitter.com/0xgitgat"
+            
+            image={Sangeet}
+          />
+        </div>
+        <div className="lg:w-1/3 md:w-full  mb-8">
+          <TeamMember
+            name="Madhuja Joshi"
+            title="Social Media | Research and Business Development"
+            description="Madhuja has first-hand experience in local languages being barriers to efficient communication and is keen on getting work done on the ground to solve this problem."
+            linkedin="https://www.linkedin.com/in/madhuja-joshi-542856184/"
+            twitter="https://twitter.com/itsmadhujaaa"
+            
+            image={Vedant}
+          />
+        </div>
+        <div className="lg:w-1/3 md:w-full  mb-8">
+          <TeamMember
+            name="Madhuja Joshi"
+            title="Social Media | Research and Business Development"
+            description="Madhuja has first-hand experience in local languages being barriers to efficient communication and is keen on getting work done on the ground to solve this problem."
+            linkedin="https://www.linkedin.com/in/madhuja-joshi-542856184/"
+            twitter="https://twitter.com/itsmadhujaaa"
+            
+            image={Ruchit}
+          />
+        </div>
+        <div className="lg:w-1/3 md:w-full  mb-8">
+          <TeamMember
+            name="Madhuja Joshi"
+            title="Social Media | Research and Business Development"
+            description="Madhuja has first-hand experience in local languages being barriers to efficient communication and is keen on getting work done on the ground to solve this problem."
+            linkedin="https://www.linkedin.com/in/madhuja-joshi-542856184/"
+            twitter="https://twitter.com/itsmadhujaaa"
+            
+            image={Shivangi}
+          />
+        </div>
+        <div className="lg:w-1/3 md:w-full  mb-8">
+          <TeamMember
+            name="Madhuja Joshi"
+            title="Social Media | Research and Business Development"
+            description="Madhuja has first-hand experience in local languages being barriers to efficient communication and is keen on getting work done on the ground to solve this problem."
+            linkedin="https://www.linkedin.com/in/madhuja-joshi-542856184/"
+            twitter="https://twitter.com/itsmadhujaaa"
+            
+            image={Vinay}
+          />
+        </div>
+        <div className="lg:w-1/3 md:w-full  mb-8">
+          <TeamMember
+            name="Madhuja Joshi"
+            title="Social Media | Research and Business Development"
+            description="Madhuja has first-hand experience in local languages being barriers to efficient communication and is keen on getting work done on the ground to solve this problem."
+            linkedin="https://www.linkedin.com/in/madhuja-joshi-542856184/"
+            twitter="https://twitter.com/itsmadhujaaa"
+            
+            image={Tanisha}
+          />
+        </div>
+        
       </div>
     </div>
   );
